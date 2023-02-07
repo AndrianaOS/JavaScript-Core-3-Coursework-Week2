2b0b0192da7b71d371a12638215a8e01bdb78f26
Andriana <andrianasaffo@gmail.com>
2023-02-07 08:19:23 +0000

    Programmer humour complete

---
+++ challenges/programmer-humour/script.js
@@ -0,0 +1,24 @@
+async function fetchImage() {
+  try {
+    const response = await fetch("https://xkcd.now.sh/?comic=latest");
+    const objectResponse = await response.json();
+    console.log(objectResponse);
+    return objectResponse;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const imageContainer = document.querySelector("#imagecontainer");
+const buttonEl = document.querySelector("#submit");
+const myImages = document.querySelector("#myImage");
+
+buttonEl.addEventListener("click", showImages);
+
+async function showImages() {
+  imageObject = await fetchImage();
+  console.log(imageObject);
+  myImages.src = imageObject.img;
+  myImages.alt = imageObject.alt;
+  console.log(myImages);
+}

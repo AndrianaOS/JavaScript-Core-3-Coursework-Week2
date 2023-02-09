58611fc3ec0bfee5c543ca443f121f6b2f5e3f21
Andriana <andrianasaffo@gmail.com>
2023-02-09 13:40:35 +0000

    refactored code to make it shorter

--- challenges/dog-photo-gallery/script.js
+++ challenges/dog-photo-gallery/script.js
@@ -4,27 +4,36 @@ const photoGallery = document.querySelector("#photogallery");
 async function getDogImage() {
   try {
     const response = await fetch(`https://dog.ceo/api/breeds/image/random`);
-    const objectResponse = await response.json();
-    // const objectURL = URL.createObjectURL(blob);
-    return objectResponse;
+    const dogData = await response.json();
+
+    const dogImage = document.createElement("img");
+    dogImage.src = dogData.message;
+    dogImage.style.width = "20em";
+    dogImage.style.height = "20em";
+
+    const listItem = document.createElement("li");
+    photoGallery.appendChild(listItem);
+    listItem.style.listStyle = "none";
+
+    listItem.appendChild(dogImage);
   } catch (error) {
     console.log(error);
   }
 }
 
-buttonEl.addEventListener("click", showDogImage);
+buttonEl.addEventListener("click", getDogImage);
 
-async function showDogImage() {
-  const dogImage = document.createElement("img");
-  const imageObject = await getDogImage();
-  dogImage.src = imageObject.message;
-  dogImage.style.width = "20em";
-  dogImage.style.height = "20em";
-  // console.log(dogImage);
+// async function showDogImage() {
+//   const dogImage = document.createElement("img");
+//   const imageObject = await getDogImage();
+//   dogImage.src = imageObject.message;
+//   dogImage.style.width = "20em";
+//   dogImage.style.height = "20em";
+//   // console.log(dogImage);
 
-  const listItem = document.createElement("li");
-  photoGallery.appendChild(listItem);
-  listItem.style.listStyle = "none";
+//   const listItem = document.createElement("li");
+//   photoGallery.appendChild(listItem);
+//   listItem.style.listStyle = "none";
 
-  listItem.appendChild(dogImage);
-}
+//   listItem.appendChild(dogImage);
+// }
--- challenges/programmer-humour/script.js
+++ challenges/programmer-humour/script.js
@@ -1,24 +1,28 @@
+const imageContainer = document.querySelector("#imagecontainer");
+const buttonEl = document.querySelector("#submit");
+const myImages = document.querySelector("#myImage");
+
 async function fetchImage() {
   try {
     const response = await fetch("https://xkcd.now.sh/?comic=latest");
     const objectResponse = await response.json();
     console.log(objectResponse);
-    return objectResponse;
+    myImages.src = objectResponse.img;
+    myImages.alt = objectResponse.alt;
+    console.log(myImages);
   } catch (error) {
     console.log(error);
   }
 }
 
-const imageContainer = document.querySelector("#imagecontainer");
-const buttonEl = document.querySelector("#submit");
-const myImages = document.querySelector("#myImage");
+// buttonEl.addEventListener("click", showImages);
 
-buttonEl.addEventListener("click", showImages);
+// async function showImages() {
+//   const imageObject = await fetchImage();
+//   console.log(imageObject);
+//   myImages.src = imageObject.img;
+//   myImages.alt = imageObject.alt;
+//   console.log(myImages);
+// }
 
-async function showImages() {
-  const imageObject = await fetchImage();
-  console.log(imageObject);
-  myImages.src = imageObject.img;
-  myImages.alt = imageObject.alt;
-  console.log(myImages);
-}
+fetchImage();
